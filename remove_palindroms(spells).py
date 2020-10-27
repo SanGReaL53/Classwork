fb1820b12d83f029806c29f4272d8c0669da7af6
SanGReaL53 <58322074+SanGReaL53@users.noreply.github.com>
2020-10-27 21:54:25 +0300

    Add files via upload

---
+++ remove_palindroms(spells).py
@@ -0,0 +1,9 @@
+def remove_palindroms(spells):
+    bw = []
+    for x in spells:
+        word = x.replace(' ', '')
+        word = word.lower()
+        if word == word[::-1]:
+            bw.append(x)
+    for word in bw:
+        spells.remove(word)
