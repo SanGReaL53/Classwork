ee75c3aca68147e55b678f40ffd8152b14d4dff8
SanGReaL53 <58322074+SanGReaL53@users.noreply.github.com>
2020-11-01 12:22:57 +0300

    Add files via upload

---
+++ date_autumn.py
@@ -0,0 +1,16 @@
+def date_autumn(dates):
+    ml = []
+    for day in dates:
+        day = day.split('-')
+        if day[0] == '11' or day[0] == '10' or day[0] == '9':
+            ml.append(day)
+    day = ml[0]
+    maximum = int(day[0]) * 30 + int(day[1]) + int(day[2]) * 365
+    for i in range(len(ml)):
+        if int(ml[i][0]) * 30 + int(ml[i][1]) \
+            + int(ml[i][2]) * 365 > maximum:
+            maximum = int(ml[i][0]) * 30 + \
+                int(ml[i][1]) + int(ml[i][2]) * 365
+            day = ml[i]
+    return '-'.join(day)
+
