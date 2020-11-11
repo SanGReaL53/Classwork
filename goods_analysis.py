f93e43d2e89f3d0f41c457a548e4d189772bc587
SanGReaL53 <58322074+SanGReaL53@users.noreply.github.com>
2020-11-11 19:28:42 +0300

    Update goods_analysis.py

--- goods_analysis.py
+++ goods_analysis.py
@@ -1,6 +1,7 @@
 def goods_analysis(*products, in_sale=None):
     if in_sale is None:
-        products = [z for z in products if z['название'].lower().find('молоко') != -1]
+        p = [z for z in products if z['название'].lower().find('молоко') != -1]
+        products = p
     else:
         products = [z for z in products if in_sale(z)]
     return sorted(products, key=lambda z: z['цена'])[:3]
